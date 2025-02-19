deba900ef53d30b8a59157a9a80b054a75497974
Aaron Leao <aaronleao@gmail.com>
2025-02-19 20:31:05 +0100

    Fetching API_KEY from environment variable

--- steam_inventory_manager/constants.py
+++ steam_inventory_manager/constants.py
@@ -6,13 +6,13 @@ from platformdirs import PlatformDirs
 
 
 APP_NAME = "steam_inventory_manager"
-STEAM_API_KEY_USAGE = 10
+STEAM_API_KEY_USAGE_LIMIT = 10
 APP_ID = 570
 APP_ID_map = {570, "Dota 2"}
 CACHE_DIR = PlatformDirs(APP_NAME, getpass.getuser()).user_data_dir
 CONTEXT_ID = "2"  # Default context ID for most games
 INVENTORY_URL_TIMEOUT = 1000000000  # 1 second
-
+STEAM_API_KEY_env = "STEAM_API_KEY"
 
 class ItemType(Enum):
     """Enum for the item type."""
--- steam_inventory_manager/parser.py
+++ steam_inventory_manager/parser.py
@@ -2,14 +2,41 @@
 
 import argparse
 import logging
+import os
 import sys
+from steam_inventory_manager import constants
 from steam_inventory_manager import steam_api_handler
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__package__)
 
+def get_env_api_key(env:str):
+    """
+    Get the STEAM_API_KEY from environment variable
+    """
+    api_key = os.environ.get(env)
+    if api_key is None:
+        raise SystemExit(f"env {env} not set. {api_key}")
+    return api_key
+
+class CustomAction(argparse.Action):
+    """
+    Specialized action for handling --api-key arg
+    """
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("nargs not allowed in CustomAction")
+        super().__init__(option_strings, dest, **kwargs)
+    def __call__(self, parser, namespace, values, option_string=None):
+        if "api-key" in option_string:
+            setattr(namespace, self.dest, get_env_api_key(values))
+
 def check_args(args):
-    """Check the command line arguments for validity."""
+    """
+    Check the command line arguments for validity.
+    """
+
+    args.api_key = get_env_api_key(args.api_key)
     if args.steam_ids is None:
         if args.steam_users is None:
             raise SystemExit("Please provide either --profile-id or --profile-user.")
@@ -26,17 +53,19 @@ def check_args(args):
     if args.display_inventory_full and not args.display_inventory:
         args.display_inventory = True
 
+
 def get_args():
     """Parse the command line arguments."""
     # Set up argument parser
-    parser = argparse.ArgumentParser(description="Fetch Steam inventory.")
+    parser = argparse.ArgumentParser(prog="steam_inventory_manager", description="Fetch Steam Manager.")
     parser.add_argument("--steam-ids", nargs="+", type=str, help="17-digit SteamIDs.")
     parser.add_argument("--steam-users", nargs="+", type=str, help="List of users.")
     parser.add_argument(
         "--app-id", type=str, default="570", help="The app ID (Dota 2=570)."
     )
+    # API shall be fetched from env STEAM_API_KEY
     parser.add_argument(
-        "--api-key", type=str, help="Your Steam API key (optional for some games)."
+        "--api-key", default=constants.STEAM_API_KEY_env, action=CustomAction ,type=str, help=f"env variable with your Steam API key. default={constants.STEAM_API_KEY_env}"
     )
     parser.add_argument(
         "--overwrite", action="store_true", help="Overwrite the inventory files."
@@ -57,5 +86,4 @@ def get_args():
 
     args = parser.parse_args()
     check_args(args)
-    # logger.info("Args %s", vars(args))
     return args
